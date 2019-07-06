d70f6903a52cf8b01dc54f89473692eadf8f3999
Wojciech Szmidt <wojciech.szmidt@fortedigital.no>
2019-07-06 14:27:56 +0200

    Updates

---
+++ precache-manifest.d9cdfc8500c1ccac37e3fbae41873adb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d5884542000e162f5e133452724d172",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fbe56baa6f529f3a8c5",
+    "url": "/static/css/main.cae2b3f2.chunk.css"
+  },
+  {
+    "revision": "29c43cb32209cb750f00",
+    "url": "/static/js/2.20105e1f.chunk.js"
+  },
+  {
+    "revision": "9fbe56baa6f529f3a8c5",
+    "url": "/static/js/main.e625a313.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
